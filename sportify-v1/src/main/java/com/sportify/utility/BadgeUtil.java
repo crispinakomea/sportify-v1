7cff350f0f258c462d4d3bf8231534499bf88bcf
Crispin A. <Crispinakomea@gmail.com>
2018-03-23 22:19:56 +0000

    Badges resized whilst maintaining ratio

--- sportify-v1/src/main/java/com/sportify/utility/BadgeUtil.java
+++ sportify-v1/src/main/java/com/sportify/utility/BadgeUtil.java
@@ -1,35 +1,43 @@
 package com.sportify.utility;
 
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * @author Crispin A.
+ *
+ */
 public class BadgeUtil {
 
 	private static final String GENERIC_BADGE = "src/main/resources/img/badge/Generic.png";
 	private static final Logger logger = LogManager.getLogger(BadgeUtil.class);
 
-	public static ImageIcon getBadge(String path, int size) {
-		Image image = null;
+	public static ImageIcon getBadge(String path, double size) {
+		BufferedImage bufferedImage = null;
+		int scaleX;
+		int scaleY;
 		try {
-			image = ImageIO.read(new File(path));
+			bufferedImage = ImageIO.read(new File(path));
 		} catch (IOException e) {
-			logger.info(StringUtils.capitalize(path) + " does not exist. (" + e + ")");
+			logger.error(path + " does not exist. (" + e + ")");
 
 			try {
-				image = ImageIO.read(new File(GENERIC_BADGE));
+				bufferedImage = ImageIO.read(new File(GENERIC_BADGE));
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
 		}
-		image = image.getScaledInstance(size, size, Image.SCALE_SMOOTH);
+		scaleX = (int) (bufferedImage.getWidth() * size);
+		scaleY = (int) (bufferedImage.getHeight() * size);
+		Image image = bufferedImage.getScaledInstance(scaleX, scaleY, Image.SCALE_SMOOTH);
 		return new ImageIcon(image);
 	}
 
